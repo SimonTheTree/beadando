d18e26bcf57815c79f5fafaf6e4fa3dd6a7fb3e2
Manz Günter <gunterznam@gmail.com>
2017-03-24 19:49:19 +0100

    added exception UserAlreadyExistsException, UserNotFoundException

---
+++ QuizOjee/src/model/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class UserAlreadyExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2630771900776621333L;
+	
+}
---
+++ QuizOjee/src/model/exceptions/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class UserNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7085106559946284312L;
+
+}
